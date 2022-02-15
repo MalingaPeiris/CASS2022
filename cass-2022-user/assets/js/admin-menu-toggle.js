ee99043262ec922b5d191a891c27d05f5d09374d
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-02-16 05:21:32 +0530

    basic-content

--- cass-2022-user/assets/js/admin-menu-toggle.js
+++
@@ -1,8 +0,0 @@
-let toggle = document.querySelector('.admin-toggle');
-let navigation = document.querySelector('.admin-navigation');
-let main = document.querySelector('.admin-main');
-
-toggle.onClick = function () {
-    navigation.classList.toggles('active');
-    main.classList.toggles('active');
-}
